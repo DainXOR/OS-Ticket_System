bf6e0e869ce86b5778873453153c4e1d114686ce
dainxor <daniel.leond@udea.edu.co>
2026-03-04 14:11:01 -0500

    Correction on inclusion path

--- src/ticket/ticket.c
+++ src/ticket/ticket.c
@@ -1,6 +1,6 @@
-#include "../../include/ticket/ticket.h"
+#include "ticket/ticket.h"
 
-#include "../../include/ticket/ticket_data.h"
+#include "ticket/ticket_data.h"
 
 ticket_data generateTicket(uint64_t userID, char* email){
 
