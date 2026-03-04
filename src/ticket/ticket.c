5aa77b795ffc13fabd002d551e37d6213b0c7225
dainxor <daniel.leond@udea.edu.co>
2026-03-04 14:12:25 -0500

    Imported stdint.h for consistency

--- src/ticket/ticket.c
+++ src/ticket/ticket.c
@@ -1,7 +1,9 @@
 #include "ticket/ticket.h"
-
 #include "ticket/ticket_data.h"
+#include "user_data.h"
+
+#include <stdint.h>
 
 ticket_data generateTicket(uint64_t userID, char* email){
-
+	return ticket_data{};
 }
